6cdd2860005c2ee984e2ec2ee3a918089cbd5ed8
chejeanx <chejeanx@hotmail.com>
2016-03-28 19:34:44 -0400

    Parking lot fees can be adjusted annually by player on budget dialog pop-up

--- src/micropolisj/gui/BudgetDialog.java
+++ src/micropolisj/gui/BudgetDialog.java
@@ -27,6 +27,7 @@ public class BudgetDialog extends JDialog
 	double origRoadPct;
 	double origFirePct;
 	double origPolicePct;
+	double origParkingPct;
 
 	JLabel roadFundRequest = new JLabel();
 	JLabel roadFundAlloc = new JLabel();
@@ -39,6 +40,10 @@ public class BudgetDialog extends JDialog
 	JLabel fireFundRequest = new JLabel();
 	JLabel fireFundAlloc = new JLabel();
 	JSlider fireFundEntry;
+	
+	JLabel parkingFundRequest = new JLabel();
+	JLabel parkingFundAlloc = new JLabel();
+	JSlider parkingFundEntry;
 
 	JLabel taxRevenueLbl = new JLabel();
 
@@ -53,11 +58,13 @@ public class BudgetDialog extends JDialog
 		int newRoadPct = ((Number) roadFundEntry.getValue()).intValue();
 		int newPolicePct = ((Number) policeFundEntry.getValue()).intValue();
 		int newFirePct = ((Number) fireFundEntry.getValue()).intValue();
+		int newParkingPct = ((Number) parkingFundEntry.getValue()).intValue();
 
 		engine.cityTax = newTaxRate;
 		engine.roadPercent = (double)newRoadPct / 100.0;
 		engine.policePercent = (double)newPolicePct / 100.0;
 		engine.firePercent = (double)newFirePct / 100.0;
+		engine.parkingPercent = (double)newParkingPct / 100.0;
 
 		loadBudgetNumbers(false);
 	}
@@ -71,6 +78,7 @@ public class BudgetDialog extends JDialog
 		roadFundEntry.setValue((int)Math.round(b.roadPercent*100.0));
 		policeFundEntry.setValue((int)Math.round(b.policePercent*100.0));
 		fireFundEntry.setValue((int)Math.round(b.firePercent*100.0));
+		parkingFundEntry.setValue((int)Math.round(b.parkingPercent*100.0));
 		}
 
 		taxRevenueLbl.setText(formatFunds(b.taxIncome));
@@ -83,6 +91,10 @@ public class BudgetDialog extends JDialog
 
 		fireFundRequest.setText(formatFunds(b.fireRequest));
 		fireFundAlloc.setText(formatFunds(b.fireFunded));
+		
+		parkingFundRequest.setText(formatFunds(b.parkingRequest));
+		parkingFundAlloc.setText(formatFunds(b.parkingFunded));
+
 	}
 
 	static void adjustSliderSize(JSlider slider)
@@ -103,6 +115,7 @@ public class BudgetDialog extends JDialog
 		this.origRoadPct = engine.roadPercent;
 		this.origFirePct = engine.firePercent;
 		this.origPolicePct = engine.policePercent;
+		this.origParkingPct = engine.parkingPercent;
 
 		// give text fields of the fund-level spinners a minimum size
 		taxRateEntry = new JSpinner(new SpinnerNumberModel(7,0,20,1));
@@ -114,6 +127,8 @@ public class BudgetDialog extends JDialog
 		adjustSliderSize(fireFundEntry);
 		policeFundEntry = new JSlider(JSlider.HORIZONTAL, 0, 100, 100);
 		adjustSliderSize(policeFundEntry);
+		parkingFundEntry = new JSlider(JSlider.HORIZONTAL, 0, 100, 100);
+		adjustSliderSize(parkingFundEntry);
 
 		ChangeListener change = new ChangeListener() {
 		public void stateChanged(ChangeEvent ev) {
@@ -124,6 +139,7 @@ public class BudgetDialog extends JDialog
 		roadFundEntry.addChangeListener(change);
 		fireFundEntry.addChangeListener(change);
 		policeFundEntry.addChangeListener(change);
+		parkingFundEntry.addChangeListener(change);
 
 		Box mainBox = new Box(BoxLayout.Y_AXIS);
 		mainBox.setBorder(BorderFactory.createEmptyBorder(8,8,8,8));
@@ -233,6 +249,12 @@ public class BudgetDialog extends JDialog
 		fundingRatesPane.add(fireFundEntry, c1);
 		fundingRatesPane.add(fireFundRequest, c2);
 		fundingRatesPane.add(fireFundAlloc, c3);
+		
+		c0.gridy = c1.gridy = c2.gridy = c3.gridy = 4;
+		fundingRatesPane.add(new JLabel(strings.getString("budgetdlg.parking_fund")), c0);
+		fundingRatesPane.add(parkingFundEntry, c1);
+		fundingRatesPane.add(parkingFundRequest, c2);
+		fundingRatesPane.add(parkingFundAlloc, c3);
 
 		return fundingRatesPane;
 	}
@@ -311,6 +333,7 @@ public class BudgetDialog extends JDialog
 		engine.roadPercent = this.origRoadPct;
 		engine.firePercent = this.origFirePct;
 		engine.policePercent = this.origPolicePct;
+		engine.parkingPercent = this.origParkingPct;
 		loadBudgetNumbers(true);
 	}
 
